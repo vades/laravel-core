be33c099ca738ea600956a6f1de5d1966fa6bb4f
martin.vach <martin.vach@helmsauer-gruppe.de>
2026-02-10 15:59:24 +0100

    #5: Adding categories-dropdown

--- resources/views/components/default/partials/header/index.blade.php
+++ resources/views/components/default/partials/header/index.blade.php
@@ -1,10 +1,18 @@
-<header id="header" class="text-header bg-bcg-header shadow-md">
+<header id="header"
+        class="text-header bg-bcg-header shadow-md">
     <section class="container mx-auto flex justify-between items-center px-4 h-16">
         <div>
-            <x-default.partials.header.brand class="flex items-center gap-8"/>
+            <x-default.partials.header.brand class="flex items-center gap-8" />
         </div>
         <div class="flex-1 mx-4">
-            <livewire:widgets.search-suggestion :contentType="['article']" :placeholderText="'Search in blog'"/>
+            <livewire:widgets.search-suggestion :contentType="['article']"
+                                                :placeholderText="'Search in blog'" />
+
+        </div>
+        <div>
+            <livewire:widgets.categories-dropdown type="article"
+                                                  route="articleIndex"
+                                                  label="articleCategories" />
         </div>
         <div>
             <x-default.partials.header.nav />
