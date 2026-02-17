5129c401944773f8484592a87fd54ee833a3904f
martin.vach <martin.vach@helmsauer-gruppe.de>
2026-02-17 13:11:53 +0100

    #11: Adding basic layout styles

--- resources/views/components/default/layout.blade.php
+++ resources/views/components/default/layout.blade.php
@@ -11,7 +11,8 @@
     <title>{{ $title ??  config('myapp.metaTitle') }}</title>
 
     <!-- Canonical URL -->
-    <link rel="canonical" href="{{ rtrim(config('app.url'), '/') . '/' . ltrim(request()->getPathInfo(), '/') . (request()->getQueryString() ? '?' . request()->getQueryString() : '') }}" />
+    <link rel="canonical"
+          href="{{ rtrim(config('app.url'), '/') . '/' . ltrim(request()->getPathInfo(), '/') . (request()->getQueryString() ? '?' . request()->getQueryString() : '') }}" />
 
     <!-- Styles / Scripts -->
     <x-shared.gtag />
@@ -21,38 +22,35 @@
         @vite([$globalCssPath, 'resources/js/app.js'])
     @endif
     @livewireStyles
-   {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>--}}
+    {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>--}}
 </head>
 <body class="{{ str_replace('/', '-', request()->path()) }}">
 <div id="root">
-{{-- #region Header --}}
-<header id="header">
-    <section class="header-container">
-        <div>
+    {{-- #region Header --}}
+    <header id="header">
+        <section class="header-container">
             <x-default.partials.header.brand class="header-brand" />
-        </div>
-        <div class="header-search">
-            <livewire:widgets.search-suggestion :contentType="['article']"
-                                                :placeholderText="__('app.search.blog')" />
+            <div class="header-search">
+                <livewire:widgets.search-suggestion :contentType="['article']" :placeholderText="__('app.search.blog')" />
 
-        </div>
-        <div>
-            <livewire:widgets.categories-dropdown type="article"
-                                                  route="articleIndex"
-                                                  label="app.nav.categories" />
-        </div>
-        <div>
-            <x-default.partials.header.nav class="header-nav"/>
-        </div>
-    </section>
-</header>
-{{-- #endregion Header --}}
- @if(isset($jumbotron) && !empty($jumbotron))
-    <section>{{ $jumbotron }}</section>
-@endif
-<main>
-    {{ $slot }}
-</main>
+            </div>
+            <div>
+                <livewire:widgets.categories-dropdown type="article"
+                                                      route="articleIndex"
+                                                      label="app.nav.categories" />
+            </div>
+            <div>
+                <x-default.partials.header.nav class="header-nav" />
+            </div>
+        </section>
+    </header>
+    {{-- #endregion Header --}}
+    @if(isset($jumbotron) && !empty($jumbotron))
+        <section>{{ $jumbotron }}</section>
+    @endif
+    <main>
+        {{ $slot }}
+    </main>
     <section id="supplementary">
         <div class="supplementary-container">
             suplementary
@@ -63,12 +61,12 @@
 
     </section>
 
-<footer id="footer">
-    <section class="footer-container">
-        <x-default.partials.footer.brand class="footer-brand"/>
-        <x-default.partials.footer.nav class="footer-nav" />
-    </section>
-</footer>
+    <footer id="footer">
+        <section class="footer-container">
+            <x-default.partials.footer.brand class="footer-brand" />
+            <x-default.partials.footer.nav class="footer-nav" />
+        </section>
+    </footer>
 
 </div>
 @livewireScripts
--- resources/views/components/default/partials/header/brand.blade.php
+++ resources/views/components/default/partials/header/brand.blade.php
@@ -1,3 +1,4 @@
 <div {{$attributes->class([])}}>
-     <a class="flex items-center gap-8" href="{{ route('home') }}"> <x-shared.img-svg img="laravel-core-logo" classList="[&>*]:w-28" /> <span class="slogan">{{ $globalNav['slogan'] }}</span></a>
+     <a class="header-logo" href="{{ route('home') }}"> <x-shared.img-svg img="laravel-core-logo" /></a>
+     <span class="header-slogan">{{ $globalNav['slogan'] }}</span>
 </div>
