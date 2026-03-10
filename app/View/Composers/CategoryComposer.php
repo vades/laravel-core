419eb9db4af9de64bc8c9d1bcfbdb728034d2eed
martin.vach <martin.vach@helmsauer-gruppe.de>
2026-03-10 10:06:01 +0100

    #12: Updating tags and categories cache

--- app/View/Composers/CategoryComposer.php
+++ app/View/Composers/CategoryComposer.php
@@ -5,25 +5,25 @@ declare(strict_types=1);
 namespace App\View\Composers;
 
 use App\Models\Category;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class CategoryComposer
 {
-    private static array $cache = [];
 
     public function compose(View $view)
     {
         $categoryType = $view->getData()['categoryType'] ?? '';
 
-        if (!isset(static::$cache[$categoryType])) {
-            static::$cache[$categoryType] = Category::publishedByType($categoryType)
-                                                    ->withCount('contents')
-                                                    ->where('contents_count', '>', 0)
-                                                    ->get();
-        }
+        $categories = Cache::remember("categories.{$categoryType}", now()->addDay(), function () use ($categoryType) {
+            return Category::publishedByType($categoryType)
+                           ->withCount('contents')
+                           ->where('contents_count', '>', 0)
+                           ->get();
+        });
 
         $view->with([
-                        'composerCategories' => static::$cache[$categoryType],
+                        'composerCategories' => $categories ,
                         'composerCurrentCategory' => request()->query('category'),
                     ]);
     }
--- app/View/Composers/TagComposer.php
+++ app/View/Composers/TagComposer.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\View\Composers;
 
+use App\Models\Category;
 use App\Models\Tag;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class TagComposer
 {
-    private static array $cache = [];
-
     public function compose(View $view)
     {
         // 1. Retrieve the data passed to the view
@@ -19,18 +19,19 @@ class TagComposer
         // 2. Extract 'type', defaulting to 'place' if missing to prevent errors
         $tagType = $viewData['tagType'] ?? '';
 
-        // 3. Use cached query result, keyed by type
-        if (!isset(static::$cache[$tagType])) {
-            static::$cache[$tagType] = Tag::byContentType($tagType)
-                                          ->withCount('contents')
-                                          ->where('contents_count', '>', 0)
-                                          ->get();
-        }
+        // 3. Use the dynamic $type variable in your query
+
+        $tags = Cache::remember("tags.{$tagType}", now()->addDay(), function () use ($tagType) {
+            return Tag::byContentType($tagType)
+                           ->withCount('contents')
+                           ->where('contents_count', '>', 0)
+                           ->get();
+        });
 
         $currentTag = request()->query('category', null);
 
         $view->with([
-                        'composerTags' => static::$cache[$tagType],
+                        'composerTags' => $tags,
                         'composerCurrentTag' => $currentTag,
                     ]);
     }
