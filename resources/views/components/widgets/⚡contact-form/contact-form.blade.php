26330465268686edf2106e29a5f0d19cc4d93de9
Martin Vach <vades.cz@gmail.com>
2026-05-12 21:53:06 +0200

    Updating contact form

--- resources/views/components/widgets/⚡contact-form/contact-form.blade.php
+++ resources/views/components/widgets/⚡contact-form/contact-form.blade.php
@@ -5,59 +5,50 @@
         </div>
     @endif
     @if (session()->has('success'))
-            <div role="alert" class="alert alert-success alert-soft">
-                <span>{{ session('success') }}</span>
-            </div>
-    @endif
-    <form wire:submit.prevent="submit">
-        <fieldset class="fieldset">
-            <label class="floating-label">
-                <input type="text" placeholder="{{__('app.form.name')}}" class="input input-md"  wire:model="name" />
-                
-            </label>
-       
-      
-        <x-ui.field >
-            <x-ui.label>{{__('app.form.name')}}</x-ui.label>
-            <x-ui.input
-                    wire:model="name"
-                    type="text"
-                    placeholder="{{__('app.form.yourName')}}"
-            />
-            <x-ui.error name="name" />
-        </x-ui.field>
-
-         <label class="floating-label">
-                <input type="text" placeholder="{{__('app.form.email')}}" class="input input-md"  wire:model="email" />
-                
-            </label>
-
-        <x-ui.field>
-           <x-ui.label>{{__('app.form.email')}}</x-ui.label>
-            <x-ui.input
-                    wire:model="email"
-                    type="text"
-                    placeholder="{{__('app.form.yourEmail')}}"
-            />
-            <x-ui.error name="email" />
-        </x-ui.field>
-         <label class="floating-label">
-        <textarea class="textarea" placeholder="{{__('app.form.message')}}" wire:model="message"></textarea>
-         </label>
-        <x-ui.field>
-            <x-ui.label>{{__('app.form.message')}}</x-ui.label>
-            <x-ui.textarea
-                    wire:model="message"
-                    placeholder="{{__('app.form.leaveMessage')}}"
-            />
-            <x-ui.error name="message" />
-        </x-ui.field>
-         </fieldset>
-
-         <fieldset class="fieldset">
-        <div class="flex justify-center items-center gap-4">
-            <a type="submit" class="btn btn-wide btn-primary">{{__('app.form.submit')}}</a>
+        <div role="alert" class="alert alert-success alert-soft">
+            <span>{{ session('success') }}</span>
         </div>
-         </fieldset>
+    @endif
+    <form wire:submit="submit">
+        <fieldset class="my-fieldset">
+            <div class="my-form-field has-w-full">
+                <label class="floating-label">
+                    <span>{{__('app.form.name')}}</span>
+                    <input type="text" id="name" placeholder="{{__('app.form.name')}}" class="input input-md"
+                           wire:model="name"/>
+
+                </label>
+                @error('name')
+                <span class="my-form-error">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="my-form-field has-w-full">
+                <label class="floating-label">
+                    <span>{{__('app.form.email')}}</span>
+                    <input type="text" id="email" placeholder="{{__('app.form.email')}}" class="input input-md"
+                           wire:model="email"/>
+
+                </label>
+                @error('email')
+                <span class="my-form-error">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="my-form-field has-w-full">
+                <label class="floating-label">
+                    <span>{{__('app.form.message')}}</span>
+                    <textarea class="textarea"  id="message" placeholder="{{__('app.form.message')}}" wire:model="message"></textarea>
+                </label>
+                @error('message')
+                <span class="my-form-error">{{ $message }}</span>
+                @enderror
+            </div>
+        </fieldset>
+
+        <fieldset class="my-fieldset">
+            <div class="my-form-submit">
+                <button type="submit" class="btn btn-wide btn-primary">{{__('app.form.submit')}}</button>
+            </div>
+        </fieldset>
     </form>
 </div>
