5d141497bf61411b1e8ba1d2681a23034eaf6316
Martin Vach <vades.cz@gmail.com>
2026-05-26 21:17:18 +0200

    Updating dropdown

--- resources/views/components/widgets/⚡search-suggestion/_search-suggestion.blade.php
+++ resources/views/components/widgets/⚡search-suggestion/_search-suggestion.blade.php
@@ -1,32 +1,26 @@
-<x-shared.dropdown>
+<x-ui.my-dropdown>
     <x-slot name="header">
 
         <div class="relative">
-            <div class="search-suggestions-container">
-                <x-shared.img-svg img="search"
-                                 classList="search-suggestions-icon" />
-            </div>
-            <input
-                    type="text"
-                    class="form-input has-icon-end"
-                    placeholder="{{$placeholderText}}."
+            <label class="input w-full input-md input-primary">
+                 <x-ui.my-img-svg img="search" classList="my-icon" />
+                <input type="search" placeholder="{{$placeholderText}}"
                     wire:model.live.debounce.300ms="query"
                     wire:keydown.escape="reset"
                     wire:keydown.tab="reset"
                     wire:keydown.enter="selectResult"
                     wire:keydown.arrow-down="moveSelectionDown"
-                    wire:keydown.arrow-up="moveSelectionUp"
-            />
+                    wire:keydown.arrow-up="moveSelectionUp" />
+            </label>
         </div>
     </x-slot>
     {{-- The dropdown with the search results.
             It is only shown if the query is not empty and there are search results. --}}
     @if(!empty($query) && !empty($results))
-        <x-slot name="body"
-                class="dropdown">
-            <ul class="dropdown-container">
+        <x-slot name="body" class="w-full">
+            <ul>
                 @foreach($results as $key => $value)
-                    <li class="dropdown-list-item {{ $selectedResult === $key ? 'is-selected' : '' }}">
+                    <li class="block px-4 py-2 hover:dark:text-neutral-100 hover:bg-neutral-100 hover:dark:bg-neutral-700 cursor-pointer text-lg {{ $selectedResult === $key ? 'is-selected' : '' }}">
                         <a
                                 href="#"
                                 wire:click.prevent="selectResult({{ $value->id }})"
@@ -37,4 +31,4 @@
             </ul>
         </x-slot>
     @endif
-</x-shared.dropdown>
+</x-ui.my-dropdown>
--- resources/views/components/widgets/⚡search-suggestion/search-suggestion.blade.php
+++ resources/views/components/widgets/⚡search-suggestion/search-suggestion.blade.php
@@ -1,34 +1,33 @@
-<x-ui.my-dropdown>
-    <x-slot name="header">
+<div class="dropdown dropdown-open my-search-dropdown">
+<label class="input w-full input-md input-primary">
+    <x-ui.my-img-svg img="search" classList="my-icon"/>
+    <input type="search" placeholder="{{$placeholderText}}"
+           wire:model.live.debounce.300ms="query"
+           wire:keydown.escape="reset"
+           wire:keydown.tab="reset"
+           wire:keydown.enter="selectResult"
+           wire:keydown.arrow-down="moveSelectionDown"
+           wire:keydown.arrow-up="moveSelectionUp"/>
+</label>
 
-        <div class="relative">
-            <label class="input w-full input-md input-primary">
-                 <x-ui.my-img-svg img="search" classList="my-icon" />
-                <input type="search" placeholder="{{$placeholderText}}"
-                    wire:model.live.debounce.300ms="query"
-                    wire:keydown.escape="reset"
-                    wire:keydown.tab="reset"
-                    wire:keydown.enter="selectResult"
-                    wire:keydown.arrow-down="moveSelectionDown"
-                    wire:keydown.arrow-up="moveSelectionUp" />
-            </label>
-        </div>
-    </x-slot>
-    {{-- The dropdown with the search results.
-            It is only shown if the query is not empty and there are search results. --}}
-    @if(!empty($query) && !empty($results))
-        <x-slot name="body" class="w-full">
-            <ul>
-                @foreach($results as $key => $value)
-                    <li class="block px-4 py-2 hover:dark:text-neutral-100 hover:bg-neutral-100 hover:dark:bg-neutral-700 cursor-pointer text-lg {{ $selectedResult === $key ? 'is-selected' : '' }}">
-                        <a
-                                href="#"
-                                wire:click.prevent="selectResult({{ $value->id }})"
-                                @if($selectedResult === $key) style="font-weight:bold;" @endif
-                        >{{ $value->title }}</a>
-                    </li>
-                @endforeach
-            </ul>
-        </x-slot>
+
+{{-- The dropdown with the search results.
+        It is only shown if the query is not empty and there are search results. --}}
+@if(!empty($query) && !empty($results))
+
+        <ul tabindex="-1" class="dropdown-content">
+        @foreach($results as $key => $value)
+            <li>
+                <a
+                    href="#"
+                    wire:click.prevent="selectResult({{ $value->id }})"
+                    @if($selectedResult === $key) style="font-weight:bold;" @endif
+                >{{ $value->title }}</a>
+            </li>
+        @endforeach
+    </ul>
+
     @endif
-</x-ui.my-dropdown>
+
+
+</div>
